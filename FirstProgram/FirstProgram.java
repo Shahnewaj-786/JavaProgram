7ccb3a49f213ecbd3335c71d957da6f0eae7b35b
Shahnewaj-786 <shahnewajshaikh786@gmail.com>
2024-12-06 01:22:25 +0600

    Print Text

---
+++ FirstProgram/FirstProgram.java
@@ -0,0 +1,5 @@
+class FirstProgram { //in java file name and class name should be same
+    public static void main(String args[]) {
+        System.out.println("This is my java first program.");
+    }
+}
