d7074aafc9f73a4952cb279917b5366d9d5553a2
Shahnewaj-786 <shahnewajshaikh786@gmail.com>
2024-12-07 00:39:42 +0600

    -

--- FirstProgram/FirstProgram.java
+++ FirstProgram/FirstProgram.java
@@ -2,7 +2,7 @@ class FirstProgram { //in java file name and class name should be same
     public static void main(String args[]) {
         System.out.println("This is my java first program.");
 
-        System.out.println("\' This is ecsape sequence \'");
+        System.out.println("\' This is escape sequence \'");
         System.out.println("This is First java Program \n Learning new program");
     }
 }
