a8c1bf2a61d994ff2f94c6ee2d705f2d617c24d5
Shahnewaj-786 <shahnewajshaikh786@gmail.com>
2024-12-07 00:38:34 +0600

    Data type and variable in java

---
+++ FirstProgram/Variable.java
@@ -0,0 +1,27 @@
+class Variable {
+    public static void main(String[] args) {
+        //For java data type must be include with variable name
+        int num = 1;
+        float num2 = 9.5f;
+        boolean Boolean = true;
+        char Char = 'T';
+        short Short = 5625;
+        double Double = 56.5;
+
+        /*
+        Double = 20.5;
+        Short = 32366;
+        //Char = "J";
+        Boolean = false;
+        * */
+
+        System.out.println("numn is integer type: " + num);
+        System.out.println("numn2 is flot type: " + num2);
+        System.out.println("Boolean type: " + Boolean);
+        System.out.println("char type: " + Char);
+        System.out.println("short type: " + Short);
+        System.out.println("Double type: " + Double);
+
+
+    }
+}
