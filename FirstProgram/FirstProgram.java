e8fe92d1df0d7c077bb308f323e8ed95f95b18e1
Shahnewaj-786 <shahnewajshaikh786@gmail.com>
2024-12-07 00:14:51 +0600

    Use Escape Sequence

--- FirstProgram/FirstProgram.java
+++ FirstProgram/FirstProgram.java
@@ -1,5 +1,8 @@
 class FirstProgram { //in java file name and class name should be same
     public static void main(String args[]) {
         System.out.println("This is my java first program.");
+
+        System.out.println("\' This is ecsape sequence \'");
+        System.out.println("This is First java Program \n Learning new program");
     }
 }
