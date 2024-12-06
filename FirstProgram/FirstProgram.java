e61666b253ce1043eb316024b50d4d7bc8c83f58
Shahnewaj-786 <shahnewajshaikh786@gmail.com>
2024-12-07 00:43:48 +0600

    Escape Sequence

--- FirstProgram/FirstProgram.java
+++ FirstProgram/FirstProgram.java
@@ -3,6 +3,9 @@ class FirstProgram { //in java file name and class name should be same
         System.out.println("This is my java first program.");
 
         System.out.println("\' This is escape sequence \'");
-        System.out.println("This is First java Program \n Learning new program");
+        System.out.println("This is First java Program \n Learning new program \t Escape Sequence");
+        System.out.println("1 \t 2");
+        System.out.println("3 \t 4");
+
     }
 }
